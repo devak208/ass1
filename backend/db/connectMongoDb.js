11fcae2451449ffa5778bc162b662875f5d316ee
devak208 <devakdevak208@gmail.com>
2024-11-30 19:51:51 +0530

    aa

--- backend/db/connectMongoDb.js
+++ backend/db/connectMongoDb.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose')
 
 exports.connectMongoDb = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_DB_URL,)
+        await mongoose.connect(process.env.MONGO_DB_URL,
+            { useUnifiedTopology: true }
+        )
         console.log("connected to mongoDb");
         
     } catch (error) {
