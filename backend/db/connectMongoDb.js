df6460a60f1735221ebfb3f1c8c820d53adc8adf
devak208 <devakdevak208@gmail.com>
2024-11-30 20:20:06 +0530

    aa

--- backend/db/connectMongoDb.js
+++ backend/db/connectMongoDb.js
@@ -1,16 +1,13 @@
 const mongoose = require('mongoose')
 
-
-const connectMongoDb = async () => {
+exports.connectMongoDb = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-        console.log("MongoDB Connected...");
+        await mongoose.connect(process.env.MONGO_DB_URL,)
+        console.log("connected to mongoDb");
+        
     } catch (error) {
-        console.error("MongoDB connection failed:", error);
-        process.exit(1);
+        console.log("Error connecting to mongoDb" + error);
+
     }
-};
-
-module.exports = connectMongoDb; // Correct way to export
-
+}
 
