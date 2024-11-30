c5e69f1a1713e1970c82d3c635e9e6376c023709
devak208 <devakdevak208@gmail.com>
2024-11-30 19:47:46 +0530

    aa

--- backend/db/connectMongoDb.js
+++ backend/db/connectMongoDb.js
@@ -1,20 +1,13 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
 exports.connectMongoDb = async () => {
     try {
-        // Establish connection with recommended options
-        await mongoose.connect(process.env.MONGO_DB_URL, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true, // Ensures a stable connection pool
-            
-            
-        });
-
-        console.log("Successfully connected to MongoDB");
+        await mongoose.connect(process.env.MONGO_DB_URL,)
+        console.log("connected to mongoDb");
+        
     } catch (error) {
-        console.error("Error connecting to MongoDB:", error.message);
+        console.log("Error connecting to mongoDb" + error);
 
-        // Optional: Exit process if database connection fails
-        process.exit(1);
     }
-};
+}
+
